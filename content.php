a509524077efd2f552ca86fd710abc0c857760a4
lil988 <53288044+lil988@users.noreply.github.com>
2019-08-06 14:22:11 -0700

    Add files via upload

---
+++ content.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+
+<!--
+File Name: index.html
+Date: 07/29/2019
+Programmer: Juana Maquin
+-->
+
+<html lang="en">
+
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8">
+	<meta name="author" content="Juana Maquin">
+	<base target="_blank"> <!-- open all links not marked "_self" in a new tab -->
+	<title>Ace in the Hole Multisport Events</title>
+
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,700' rel='stylesheet' type='text/css'>
+	<link href="css/reset.css" rel="stylesheet" type="text/css">
+	<link href="css/grid.css" rel="stylesheet" type="text/css">
+	<link href="css/style.css" rel="stylesheet" type="text/css">
+	<link href="css/navigation.css" rel="stylesheet" type="text/css">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.7.1/less.min.js"></script>
+	<script defer src="https://use.fontawesome.com/releases/v5.5.0/js/all.js" integrity="sha384-GqVMZRt5Gn7tB9D9q7ONtcp4gtHIUEW/yG7h98J7IpE3kpi+srfFyyB/04OV6pG0" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+
+	<!---here goes the header--->
+	<?php
+    include 'header.php';
+    ?>
+
+	<main>
+		<section id="common1" class="section group">
+			<div class="col span_1_of_2 span_2_of_2">
+				<h2>Ace in the Hole Multisport Events (h2)</h2>
+				<br />
+				<p>(Paragraph text) auctor et lacinia id, venenatis nec purus. Phasellus ut vehicula lectus, eu convallis arcu. Donec eu sapien quis odio suscipit bibendum quis nec mauris. Quisque vestibulum neque sapien. Nunc sollicitudin metus accumsan fringilla sollicitudin. Ut arcu felis, maximus a sem at, vulputate suscipit leo. Duis in sapien turpis. Pellentesque tortor nulla, vehicula ut fermentum nec, faucibus quis felis. Mauris ut euismod eros.
+					Ut id tincidunt sapien. Phasellus id ullamcorper odio, nec vehicula ligula.
+					Donec maximus enim sed auctor rutrum. Fusce et diam non ipsum placerat tincidunt dignissim at mauris. Etiam vitae finibus nibh, a aliquam neque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae ipsum eu dolor suscipit fringilla. Nullam non magna eu nisl vulputate suscipit. Sed at odio auctor, ornare ante eu, ultrices nunc. Fusce lobortis dictum neque vitae dignissim.</p>
+				Nullam non magna eu nisl vulputate suscipit. Sed at odio auctor, ornare ante eu, ultrices nunc. Fusce lobortis dictum neque vitae Quisque vestibulum neque sapien. Nunc sollicitudin metus accumsan fringilla sollicitudin. Ut arcu felis, maximus a sem at, vulputate suscipit leo. Duis in sapien turpis. Pellentesque tortor nulla, vehicula ut fermentum nec, faucibus quis felis. Mauris ut euismod eros.
+				Ut id tincidunt sapien. Phasellus id ullamcorper odio, nec vehicula ligula.(Paragraph text).
+			</div>
+			<div class="col span_1_of_2 span_2_of_2">
+				<h2>Ace in the Hole Multisport Events Images (h2)</h2>
+				<p>(Images below)</p>
+				<img src="images/back_Image.jpg" alt="" />
+			</div>
+		</section>
+		<br />
+		<section id="references1" class="section group">
+			<div class="col span_1_of_2 span_2_of_2">
+				<h2>Content of other pages below</h2>
+				<div class="col span_1_of_2 span_1_of_3_of_2">
+					<h3><a>Form for Registers (Events Page)</a></h3>
+					<ul>
+						<a>Registration Form</a>
+					</ul>
+				</div>
+				<div class="col span_1_of_2 span_1_of_3_of_2">
+					<h3><a>Form of Contact Page</a></h3>
+					<ul>
+						<a>Contact Form</a>
+					</ul>
+				</div>
+		</section>
+	</main>
+	<footer>
+		<p>Ace in the Hole Multisport Events &copy; 2019</p>
+		<p id="contact1">Juana Maquin - PCC - <a href="https://www.pcc.edu/">Portland Community College</a></p>
+	</footer>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,37 @@
+<?php
+
+?>
+
+<header>
+	<div id="company-logo">
+		<h1>Ace in the Hole Multisport Events</h1>
+	</div>
+	<div id="social-icon">
+		<p>
+			<a href-"#"><i class="fab fa-facebook-square"></i></a>
+			<a href-"#"><i class="fab fa-twitter-square"></i></a>
+			<a href-"#"><i class="fab fa-instagram"></i></a>
+			<a href-"#"><i class="fab fa-youtube-square"></i></a>
+		</p>
+	</div>
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			</div>
+			<div id="navbar" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav navbar-left">
+					<li><a href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Events</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+</header>
